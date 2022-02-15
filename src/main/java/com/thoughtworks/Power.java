06f6527929527f2f9ccf3e35bddb372d29b7277e
Varun <varsho2002@gmail.com>
2022-02-15 15:48:30 +0530

    Refactor method names

--- src/main/java/com/thoughtworks/Power.java
+++ src/main/java/com/thoughtworks/Power.java
@@ -1,7 +1,7 @@
 package com.thoughtworks;
 
 public class Power {
-    public static int OF(int xyz, int pqr) {
+    public static int of(int xyz, int pqr) {
         int p = 1;
         for (int i = 0; i<pqr; i++) {
             p *= xyz;
--- src/test/java/com/thoughtworks/PowerTest.java
+++ src/test/java/com/thoughtworks/PowerTest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 public class PowerTest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert Power.OF(1,1) == 1;
+    public void givenOneRaisedOneWhenCalculatedShouldEquateToOne() {
+        assert Power.of(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert Power.OF(2,1) == 2;
+    public void givenTwoRaisedOneWhenCalculatedShouldEquateToTwo() {
+        assert Power.of(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert Power.OF(2, 2) == 4;
+    public void givenTwoRaisedTwoWhenCalculatedShouldEquateToFour() {
+        assert Power.of(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert Power.OF(3, 2) == 3*3;
+    public void givenTwoRaisedThreeWhenCalculatedShouldEquateToSix() {
+        assert Power.of(3, 2) == 3*3;
     }
 }
